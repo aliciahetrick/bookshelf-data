b3f59f4b9fb834e2fb3a62427a72cecc2f1ac9eb
TaraTritt <aliciahetrick@gmail.com>
2022-12-20 11:20:54 -0500

    addBook() method tested of Bookshelf class

--- js/bookshelf.js
+++ js/bookshelf.js
@@ -13,6 +13,10 @@ const book1 = new Book('author', 'english', 'horror', 'horror book')
 console.log(book1)
 const book2 = new Book('author2', 'english2', 'horror2', 'horror book2')
 console.log(book2)
+const book3 = new Book('author3', 'english3', 'horror3', 'horror book3')
 
 const bookshelf1 = new Bookshelf([book1, book2])
 console.log('bookshelf', bookshelf1)
+
+const newBookshelf = bookshelf1.addBook(book3)
+console.log(newBookshelf)
