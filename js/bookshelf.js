212fc574767770e8593f0a1b4cfa84a77b46e9b1
TaraTritt <aliciahetrick@gmail.com>
2022-12-20 15:55:16 -0500

    jsdoc commented .js files

--- js/bookshelf.js
+++ js/bookshelf.js
@@ -3,17 +3,26 @@ class Bookshelf {
     this.arrOfBooks = arrOfBooks
   }
 
+  /**
+   * @returns array with newly added book
+   */
   addBook(book) {
     this.arrOfBooks.push(book)
     return this.arrOfBooks
   }
 
   render() {
+    //create booklist section
     const bookList = document.createElement('section')
+
+    //loop through all books passed
+    //render and append every book to the booklist
     for (let i = 0; i < this.arrOfBooks.length; i++) {
       const bookCard = this.arrOfBooks[i].render()
       bookList.append(bookCard)
     }
+
+    //select and append bookList to main element
     const main = document.querySelector('main')
     bookList.classList.add('book-list')
     main.append(bookList)
